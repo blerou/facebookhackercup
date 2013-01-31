6a2b89081dd63554b65de3ddf407421420013c35
blerou <sulik.szabolcs@gmail.com>
2013-01-31 23:14:11 +0100

    2012 QR - Beautiful Strings

--- 2012qr/BeautifulStrings.hs
+++ 2012qr/BeautifulStrings.hs
@@ -1,17 +1,16 @@
 import Data.Char
 import Data.List
 
-main = interact beautifulStringsSolver
+main = interact $ unlines . beautifulStringsSolver . drop 1 . lines
 
-beautifulStringsSolver :: String -> String
-beautifulStringsSolver = unlines . solveCases . drop 1 . lines
-
-solveCases :: [String] -> [String]
-solveCases cases = zipWith formatter [1..] solutions
-      where solutions = map solveBS cases
+beautifulStringsSolver :: [String] -> [String]
+beautifulStringsSolver strings = zipWith formatter [1..] solutions
+      where solutions = map beautyIndex strings
             formatter i b = "Case #" ++ show i ++ ": " ++ show b
 
-solveBS :: String -> Int
-solveBS = sum . zipWith (*) [26,25..1] . reverse . sort . map length . letterGroups 
+beautyIndex :: String -> Int
+beautyIndex = calculateIndex . occurrences . letterGroups
 
 letterGroups = group . sort . filter (`elem` ['a'..'z']) . map toLower
+occurrences = reverse . sort . map length
+calculateIndex = sum . zipWith (*) [26,25..1]
